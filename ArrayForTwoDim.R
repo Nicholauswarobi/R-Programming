bb644c7e98bfad0ff54b2171c97ec7622c1afaf8
Nicholauswarobi <warobinicholaus@gmail.com>
2025-04-03 15:57:38 +0300

    #

--- ArrayForTwoDim.R
+++ ArrayForTwoDim.R
@@ -1,5 +1,6 @@
 vector1 <- c(5, 8, 11)
 vector2 <- c(12, 45, 23, 78, 7, 4)
 
-results <- array(c(vector1, vector2, dim = c(3, 3, 2)))
+# Creating a 2D array with 3 rows and 2 columns
+results <- array(c(vector1, vector2), dim = c(3, 3, 2))
 print(results)
